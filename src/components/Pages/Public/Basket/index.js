f37cb0d67103677eaabd04d5e353ba33d1362fe0
Yurii Maystrenko <yura1994hd@gmail.com>
2019-03-04 12:52:51 +0200

    init master

--- src/components/Pages/Public/Basket/index.js
+++ src/components/Pages/Public/Basket/index.js
@@ -4,13 +4,7 @@ import Wrapper from '../../../../containers/Pages/Wrapper/index';
 
 class Basket extends Component {
   render() {
-    return (
-      <Wrapper>
-        {
-          'Basket. У звязку з тим що не працює мікросервіс кошика, з невідомих мені причин, думаю що роблю все правильно, а самого доступу до api з токеном не має, не можливо доробити завдання. Для перевірки моїх твердженнь, перейдіть \\src\\flux-saga\\bus\\fetch\\basket\\saga\\workers\\fetchBasketProducts.js - розкоментуйте код та перезапустіть зборку. Якщо це не так або виникнуть запитання - моя пошта yura1994hd@gmail.com. Якщо я не прав пишіть дороблю.'
-        }
-      </Wrapper>
-    );
+    return <Wrapper>{'Basket'}</Wrapper>;
   }
 }
 
